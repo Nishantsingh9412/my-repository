f12ad37eeb4457e3e52aa947bdfcdb3229b21f18
nishantsingh9412 <nishantsingh9412ns@gmail.com>
2020-01-11 17:54:39 +0530

    ok

---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title> Nishant Singh</title>
+
+  <!-- Font Awesome Icons -->
+  <!-- <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
+  <link rel="stylesheet" type="text/css" href="css/fontawesome.min.css"> 
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.min.css" rel="stylesheet">
+
+  <style>
+          .color{
+            background:coral;
+          }
+          .tc {
+            color:orchid;
+
+          }
+           .tc2 {
+            color:hotpink;
+
+          }
+          .txtc{
+          	color:coral;
+          }
+          .ws{
+          	width:80%;
+          	height:310px;
+          }
+           
+          
+          
+  </style>
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">  Nishant Singh</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Explore more</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold"> welcome to my portfolio </h1>
+          <hr class="divider light my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5"> Nishant Singh 
+          <br><br>	Web Designer and Developer
+          </p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Me</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section color" id="about">
+    <div class="container ab">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="text-white mt-0">  ABOUT ME
+          </h2>
+          <hr  style="width:250px; height:2px; background:white;"  >
+          <p class="text-white mb-4">  Undergraduate   <br>
+          	Working at Smartinfotechsys for 1 year. 
+           </p>
+          <a class="btn btn-light btn-xl js-scroll-trigger txtc" href="#services">explore more </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0"> Personal Information </h2>
+      <hr class="divider my-4">
+      <div class="row">
+         <div class="col-lg-12 col-md-12 text-center">
+          <div class="mt-5">
+            <i class="fa fa-4x fa-user text-primary mb-4"></i>
+            <h3 class="h4 mb-2"> About Me  </h3>
+            <br>
+            <ul class="list-group">
+              <li class="list-group-item  active text-left">Information</li>
+              <li class="list-group-item text-left"><b> Name:</b>Nishant Singh </li>
+              <li class="list-group-item text-left"><b> DOB:</b> 29/07/2001 </li>
+              <li class="list-group-item text-left"><b> Address:</b> kailash nagar kanpur</li>
+              <li class="list-group-item text-left"><b> Mobile No: </b>+91-783954XXXX </li>
+               <li class="list-group-item text-left"><b> Email:</b>ns20201@outlook.com</li>
+            </ul>
+          </div>
+        </div>
+        <br>
+        <div class="col-lg-12 col-md-12 text-center">
+          <div class="mt-5">
+            <i class="fa fa-university fa-4x  text-primary mb-4"></i>
+            <h3 class="h4 mb-2"> Education </h3>
+            <ul class="list-group">
+              
+              <li class="list-group-item  active text-left">Diploma in Web Design and Development in 2019 </li>
+              <li class="list-group-item text-left">HTML5</li>
+              <li class="list-group-item text-left">CSS3 </li>
+              <li class="list-group-item text-left">Bootstrap</li>
+              <li class="list-group-item text-left">Javascript / JQuery</li>
+              <li class="list-group-item text-left">Ajax</li> 
+              <li class="list-group-item text-left">PHP: Core PHP, PHP-OOPS </li>
+              <li class="list-group-item text-left">MYSQL</li>
+              <li class="list-group-item text-left">PHP CMS: WordPress  </li>
+              <li class="list-group-item text-left">PHP framework: Laravel  </li>
+              <li class="list-group-item text-left">Python</li>
+            </ul>
+            <br>
+            <ul class="list-group">
+              <li class="list-group-item  active text-left">Senior Secondary School</li>
+              <li class="list-group-item text-left"><b> Name of School:</b> Air Force School </li>
+              <li class="list-group-item text-left"><b> Name of Board:</b> CBSE </li>
+              <li class="list-group-item text-left"><b> Year Of Completion:</b> 2019</li>
+              <li class="list-group-item text-left"><b> Percentage: </b> 71%</li>
+            </ul>
+            <br>
+            <ul class="list-group">
+              <li class="list-group-item  active text-left">Higher Secondary School</li>
+              <li class="list-group-item text-left"><b> Name of School:</b> Air Force School </li>
+              <li class="list-group-item text-left"><b> Name of Board:</b> CBSE </li>
+              <li class="list-group-item text-left"><b> Year Of Completion:</b> 2017</li>
+              <li class="list-group-item text-left"><b> Percentage: </b> 87%</li>
+            </ul>
+          </div>
+        </div>
+     
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/html.png">
+            <img class="img-fluid w-100" src="img/portfolio/fullsize/html.png" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+               Programing Language
+              </div>
+              <div class="project-name">
+                HTML5
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/imgcss.png">
+            <img class="img-fluid" src="img/portfolio/fullsize/imgcss.png" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+               Programing Language
+              </div>
+              <div class="project-name">
+                CSS3
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/p2.png">
+            <img class="img-fluid w-100" src="img/portfolio/fullsize/p2.png" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+               Programing Language
+              </div>
+              <div class="project-name">
+                Python
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/bstrp.png">
+            <img class="img-fluid ws " src="img/portfolio/fullsize/bstrp.png" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+               Programing Language
+              </div>
+              <div class="project-name">
+                Bootstrap
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/picajax.png">
+            <img class="img-fluid ws" src="img/portfolio/fullsize/picajax.png" 
+                  alt="ajax">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+         Programing Language
+              </div>
+              <div class="project-name">
+                Ajax
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/php7.png">
+            <img class="img-fluid ws" src="img/portfolio/fullsize/php7.png" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+               Programing Language
+              </div>
+              <div class="project-name">
+                Php7
+              </div>
+            </div>
+          </a>
+        </div>
+        
+         <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/lara.png">
+            <img class="img-fluid " src="img/portfolio/fullsize/lara.png" 
+                  alt="ajax">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+         Programing Language
+              </div>
+              <div class="project-name">
+               Laravel
+              </div>
+            </div>
+          </a>
+        </div>
+         <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/js6.png">
+            <img class="img-fluid " src="img/portfolio/fullsize/js6.png" 
+                  alt="ajax">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+         Programing Language
+              </div>
+              <div class="project-name">
+                Laravel
+              </div>
+            </div>
+          </a>
+        </div>
+         <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/jq2.png">
+            <img class="img-fluid " src="img/portfolio/fullsize/jq2.png" 
+                  alt="ajax">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+         Programing Language
+              </div>
+              <div class="project-name">
+                JQuery
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action Section -->
+ <!--  <section class="page-section bg-dark text-white">
+    <div class="container text-center">
+      <h2 class="mb-4"> </h2>
+      <a class="btn btn-light btn-xl" href="https://www.naukri.com/mnjuser/profile?id=&altresid"> Hire Me for Making Your Website </a>
+    </div>
+  </section> -->
+
+  <!-- Contact Section -->
+  <section class="page-section" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Let's Get In Touch!</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5">Ready to make website or hire me on your company Give me  a call or send us an email and I will get back to you as soon as possible!</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fa fa-phone  fa-3x mb-3 text-success"></i>
+          <div class="text-success" > +91-783954XXXX </div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fa fa-envelope fa-3x mb-3 text-danger "></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <p> nishantsingh9412ns@gmail.com </p>
+        </div>
+      </div>
+      <br><br>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <!-- <i class="fab fa-instagram fa-3x mb-3 tc" > <a href=""></a></i> -->
+         <a href=" https://www.instagram.com/nishant_thakur1111/" class="tc2">  <img   src="img/portfolio/fullsize/instagram.png" width="60px" > <br>Instagram </a>
+         
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fa fa-envelope-open fa-3x mb-3 text-info "></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block text-info" href="https://outlook.live.com/owa/0/">ns20201@outlook.com</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">  <p> &copy; Designed by Nishant 
+      Singh  </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
